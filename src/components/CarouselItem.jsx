851703346c5d1f4f4a6afe0b90956aa6990fe385
Damian <damiandanielvigo@gmail.com>
2020-07-03 13:45:06 -0300

    hooks-rdy

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,16 +3,18 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
   <div className='carousel-item'>
-    <img className='carousel-item__img' src='https://placeimg.com/640/480/people' alt='People' />
+    <img className='carousel-item__img' src={cover} alt={title} />
     <div className='carousel-item__details'>
       <div>
         <img className='carousel-item__details-icons' src={playIcon} alt='Play' />
         <img className='carousel-item__details-icons' src={plusIcon} alt='Plus' />
       </div>
-      <p className='carousel-item__details--title'>Título descriptivo</p>
-      <p className='carousel-item__details--subtitle'>2020 16+ 144 minutos</p>
+      <p className='carousel-item__details--title'>{title}</p>
+      <p className='carousel-item__details--subtitle'>
+        {`${year} ${contentRating} ${duration}`}
+      </p>
     </div>
   </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/styles/App.scss';
 import Header from '../components/Header';
 import Search from '../components/Search';
@@ -6,36 +6,47 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
+const API = 'http://localhost:3000/initialState';
 
-    <Categories title='Favoritos'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+const App = () => {
+  const initialState = useInitialState(API);
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  return initialState.length === 0 ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className='App'>
+      <Header />
+      <Search />
+      {initialState.mylist?.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            {initialState.mylist.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title='Tendencias'>
+        <Carousel>
+          {initialState.trends?.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
 
-    <Categories title='Originales de Platzi Video'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+      <Categories title='Originales de Platzi Video'>
+        <Carousel>
+          {initialState.originals.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
 
-    <Footer />
-  </div>
-);
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
