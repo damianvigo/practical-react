ad5a8d9b4b7e98923f6e37ad9d455079e48986c5
Damian <damiandanielvigo@gmail.com>
2020-07-26 16:12:07 -0300

    validacion-login-logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video.png';
 import userIcon from '../assets/static/user-icon.png';
@@ -9,6 +11,11 @@ import userIcon from '../assets/static/user-icon.png';
 const Header = (props) => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
+
   return (
     <header className='header'>
       <Link to='/'>
@@ -20,12 +27,23 @@ const Header = (props) => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li>
-            <a href='/'>Cuenta</a>
-          </li>
-          <li>
-            <Link to='/login'>Iniciar sesión</Link>
-          </li>
+          {hasUser ? (
+            <li>
+              <a href='/'>{user.name}</a>
+            </li>
+          ) : null}
+
+          {hasUser ? (
+            <li>
+              <a href='#logout' onClick={handleLogout}>
+                Cerrar Sesion
+              </a>
+            </li>
+          ) : (
+            <li>
+              <Link to='/login'>Iniciar sesión</Link>
+            </li>
+          )}
         </ul>
       </div>
     </header>
@@ -38,4 +56,13 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+Header.propTypes = {
+  user: PropTypes.object,
+  logoutRequest: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
